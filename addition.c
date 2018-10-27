c89eb017c2d6b3a621025abbb4c57a1b83eee942
unknown <tannuchoudhary10@gmail.com>
2018-10-27 21:58:40 +0530

    added comment
    added comment

--- addition.c
+++ addition.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-int main() {
+int main()
+ {
     int a,b;
-    printf("enter length and breadth");
+    printf("enter length and breadth");  //length and breadth of rectangle
     scanf("%d, %d", &a, &b);
 
-    printf("\n area: %d", a*b);
+    printf("\n area: %d", a*b);          //calculating the area of rectangle.
     return 0;
 }
